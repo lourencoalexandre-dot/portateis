1056f182711e7d14a987a8554ad0fb6ccbba5871
Alexandre Lourenço <233493537+lourencoalexandre-dot@users.noreply.github.com>
2026-05-09 22:25:32 +0100

    Implement basic arithmetic functions in main.c
    Added functions for addition and multiplication.

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// TODO: Implementar esta função
+// Deve retornar a + b
+int soma(int a, int b) {
+    // COMPLETE AQUI
+}
+
+// TODO: Implementar esta função
+// Deve retornar a * b
+int multiplica(int a, int b) {
+    // COMPLETE AQUI
+}
+
+int main() {
+    printf("Soma de 5 + 3: %d\n", soma(5, 3));
+    printf("Multiplicação de 4 * 7: %d\n", multiplica(4, 7));
+    
+    return 0;
+}
